4843a629ce2ede7b98878c62967b35f1a952bc4c
Ryan Werner <rmwerner@umich.edu>
2020-05-14 02:38:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 var updateObjectWithKeyAndValue = (object, key, value) => {return Object.assign({}, object, { [key]: value})}
-var destructivelyUpdateObjectWithKeyAndValue = (objest, key, value) => {object.key = ["value"]; return object}
+var destructivelyUpdateObjectWithKeyAndValue = (object, key, value) => {object.key = ["value"]; return object}
