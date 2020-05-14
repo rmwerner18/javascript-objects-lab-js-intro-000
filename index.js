11d16e707fae28b37b781ead14da30a3fcfd7b88
Ryan Werner <rmwerner@umich.edu>
2020-05-14 02:48:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var updateObjectWithKeyAndValue = (object, key, value) => {return Object.assign({}, object, { [key]: value})}
 var destructivelyUpdateObjectWithKeyAndValue = (object, key, value) => {object[key] = value; return object}
 var deleteFromObjectByKey = (object, key) => {Object.assign({}, object); delete deleteFromObjectByKey.key; return object}
-var destructivelyDeleteFromObjectByKey = (object, key) => {delete object.key; return object}
+var destructivelyDeleteFromObjectByKey = (object, key) => {return delete object.key}
