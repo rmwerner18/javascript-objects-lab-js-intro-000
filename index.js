e969b5572b3063879d91591825765e81c1fd5d65
Ryan Werner <rmwerner@umich.edu>
2020-05-14 02:27:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-var recipes = {}
-var updateObjectWithKeyAndValue = (object, key, value) => {return Object.assign({}, object, { key: value})}
+
+var updateObjectWithKeyAndValue = (object, key, value) => {return Object.assign({}, object, { key: value })}
 var updateObjectWithKeyAndValue = (object, key, value) => {object[key] = value; return object}
